b31c7c58b2049d2954c2a958818e3cc579105ef6
Tyler Newman <tylerjnewman18@gmail.com>
2020-08-28 13:19:53 -0700

    Update logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,24 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import "../styles/styles.css"
+import logoSrc from "../images/jzl.png"
 
 const Button = () => <a href="https://www.jzlvip.tv/" className="block-button">Enter site</a>
+const Logo = () => (
+  <div
+    className="header"
+    style={{ zIndex: 1, position: "fixed", width: "100%" }}
+  >
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <img class="center" src={logoSrc} alt="Logo" style={{ width: 184 }} />
+    </div>
+  </div>
+)
 
 export default function Image() {
   const data = useStaticQuery(graphql`
@@ -21,37 +37,41 @@ export default function Image() {
   `)
 
   return (
-    <section className="page-section">
-      <div className="section-background">
-        <Img
-          style={{
-            width: "100%",
-            height: "100%",
-            objectPosition: "50% 50%",
-            objectFit: "cover",
-          }}
-          fluid={data.file.childImageSharp.fluid}
-          alt="Gatsby Docs are awesome"
-        />
-        <div className="overlay" style={{ opacity: 0.4 }} />
-      </div>
-      <div className="content-wrapper">
-        <div className="content">
-          <div className="row">
-            <h1
-              style={{
-                textAlign: "center",
-                whiteSpace: "pre-wrap",
-              }}
-            >
-              The Wildest XXX Pranks, Stunts, And Challenges Around The World!
-            </h1>
-          </div>
-          <div className="row center" >
-            <Button />
+    <>
+      <Logo />
+      <section className="page-section">
+        <div className="section-background">
+          <Img
+            style={{
+              width: "100%",
+              height: "100%",
+              objectPosition: "50% 50%",
+              objectFit: "cover",
+            }}
+            fluid={data.file.childImageSharp.fluid}
+            alt="Gatsby Docs are awesome"
+          />
+          <div className="overlay" style={{ opacity: 0.4 }} />
+        </div>
+        <div className="content-wrapper">
+          <div className="content">
+            <div className="row">
+              <h1
+                style={{
+                  textAlign: "center",
+                  whiteSpace: "pre-wrap",
+                }}
+              >
+                Exclusive videos, behind the scenes, interviews and full
+                episodes of the hottest Instagram models!
+              </h1>
+            </div>
+            <div className="row center">
+              <Button />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
